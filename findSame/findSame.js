5ae35f9ce3b6ad31e4d2b8e6421766fcab5ff44a
RobinRees <Reehees12@gmail.com>
2025-01-07 15:43:04 +0100

    Text Added for findSame

--- findSame/findSame.js
+++ findSame/findSame.js
@@ -1,4 +1,5 @@
 const resetButton = document.getElementById("resetButton");
+const findSameText = document.getElementById("findSameText");
 
 document.addEventListener("DOMContentLoaded", () => {
     const containerForCells = document.getElementById("containerForCells");
@@ -6,8 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
     containerForCells.addEventListener("click", (event) => {
         if (event.target.classList.contains("gridCell")) {
             const clickedNumber = event.target.textContent;
-
+            let counter = 0;
             const allCells = document.querySelectorAll(".gridCell");
+            
 
             for (let i = 0; i < allCells.length; i++) {
                 allCells[i].style.backgroundColor = "";
@@ -15,7 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
             for (let i = 0; i < allCells.length; i++) {
                 if (allCells[i].textContent === clickedNumber) {
+                    counter += 1;
+                    findSameText.textContent = `${counter} copies of the number ${clickedNumber}`;
                     allCells[i].style.backgroundColor = "green";
+
                 }
             }
         }
@@ -27,5 +32,6 @@ resetButton.addEventListener("click", () => {
 
     for (let i = 0; i < resetAllCells.length; i++) {
         resetAllCells[i].style.backgroundColor = "";
+        findSameText.textContent = "Click on a number to find copies of it";
     }
 })
