b28545b083d4326adee66d16e2f58e21fb6c099e
RobinRees <Reehees12@gmail.com>
2025-01-08 23:10:50 +0100

    Remade my code for remove

--- remove/remove.js
+++ remove/remove.js
@@ -1,38 +1,40 @@
-const newRandomNumber = document.getElementById("newRandomNumber");
-const randomNumberInput = document.getElementById("selectedRandomNumber");
+const newRandomNumberButton = document.getElementById("newRandomNumber");
+const randomNumberInput = document.getElementById("selectedRandomNumber")
 const removeButton = document.getElementById("removeButton");
-const removalDescription = document.getElementById("descriptionOfRemoval");
+const descriptionInput = document.getElementById("descriptionOfRemoval");
 
-newRandomNumber.addEventListener("click", () => {
-  const randomNumber = Math.floor(Math.random() * 99) + 1;
-  randomNumberInput.setAttribute("value", randomNumber);
-  const allCells = document.querySelectorAll(".gridCell");
-
-  for (let i = 0; i < allCells.length; i++) {
-    if (allCells[i].style.backgroundColor === "orange") {
-        allCells[i].style.backgroundColor = "";
-    }
-  }
-
-  for (let i = 0; i < allCells.length; i++) {
-    if (allCells[i].textContent == randomNumber) {
-      allCells[i].style.backgroundColor = "orange";
-    }
-  }
-});
-
-
-removeButton.addEventListener("click", () => {
-    let counter = 0;
-    const allCells = document.querySelectorAll(".gridCell")
-    for (let i = 0; i < allCells.length; i++) {
-        if (allCells[i].style.backgroundColor === "orange") {
-            counter += 1;
-            const saveNumber = allCells[i].textContent;
-            allCells[i].textContent = "X";
-            allCells[i].style.backgroundColor = "red";
-            removalDescription.value = `${saveNumber} was removed ${counter} times`;
+newRandomNumberButton.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * 99) + 1;
+    randomNumberInput.value = randomNumber;
+    const allCells = document.querySelectorAll(".gridCell"); 
+    
+    for (let j = 0; j < allCells.length; j++) {
+        if (allCells[j].textContent != randomNumberInput.value) {
+            allCells[j].classList.remove("orange");
         }
     }
 
+    for (let i = 0; i < allCells.length; i++) {
+        if (allCells[i].textContent == randomNumberInput.value) {
+            allCells[i].classList.add("orange");
+        }
+    }
+});
+
+removeButton.addEventListener("click", () => {
+    const allCells = document.querySelectorAll(".gridCell");
+    let counter = 0;
+    let currentNumber = 0;
+
+    for (let i = 0; i < allCells.length; i++) {
+        if (allCells[i].classList.contains("orange")) {
+            currentNumber = allCells[i].textContent;
+            allCells[i].classList.add("red");
+            allCells[i].classList.remove("orange");
+            allCells[i].textContent = "X"
+            counter++;
+        }
+
+    }
+    descriptionInput.value = `${currentNumber} was removed ${counter} time/s`;
 })
