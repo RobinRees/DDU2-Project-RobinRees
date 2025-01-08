c9b7534575ce53f1a8b624c0554e66c304bc06b5
RobinRees <Reehees12@gmail.com>
2025-01-08 17:57:34 +0100

    Js variables setup and button for Sum

---
+++ sum/sum.js
@@ -0,0 +1,3 @@
+const inputSumOfAll = document.getElementById("inputSumOfAll");
+const inputSumOfMarked = document.getElementById("inputSumOfMarked");
+const sumResetButton = document.getElementById("sumResetButton");
